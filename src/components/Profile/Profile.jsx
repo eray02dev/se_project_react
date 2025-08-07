426ba1cc28dba4bb509f3e097899d22cd8966531
eray02dev <egeeray490@gmail.com>
2025-08-07 16:16:42 -0600

    pussing the changes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,7 +2,6 @@ import "./Profile.css";
 import ItemCard from "../ItemCard/ItemCard";
 import avatar from "../../assets/avatar.png";
 import ClothesSection from "../ClothesSection/ClothesSection";
-import SideBar from "../SideBar/SideBar";
 
 function Profile({ clothingItems, onCardClick, onAddClick }) {
   return (
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,17 +1,1 @@
-import avatar from "../../assets/avatar.png";
-import "./SideBar.css";
-
-function SideBar() {
-  return (
-    <div className="sidebar">
-      <div className="sidebar__container">
-        <div className="sidebar__user">
-          <img src={avatar} alt="User avatar" className="sidebar__avatar" />
-          <h2 className="sidebar__name">Terrence Tegegne</h2>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default SideBar;
+ 
