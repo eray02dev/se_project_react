b7034c5b1fcc1909dc45c37f2a7ea99480b76c1a
eray02dev <egeeray490@gmail.com>
2025-08-07 16:25:06 -0600

    pussing the changes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,11 +2,13 @@ import "./Profile.css";
 import ItemCard from "../ItemCard/ItemCard";
 import avatar from "../../assets/avatar.png";
 import ClothesSection from "../ClothesSection/ClothesSection";
+import SideBar from "../SideBar/SideBar";
 
 function Profile({ clothingItems, onCardClick, onAddClick }) {
   return (
     <div className="profile">
       <div className="profile__container">
+        <SideBar />
         <div className="profile__user">
           <img src={avatar} alt="User avatar" className="profile__avatar" />
           <h2 className="profile__name">Terrence Tegegne</h2>
